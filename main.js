2b388376f457984ba9b1a16f62209fafe9f3e8b6
ryochana <ryochana98@gmail.com>
2025-07-20 04:30:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // CONFIGURATION
 // ================================================================
 const SUPABASE_URL = 'https://nmykdendjmttjvvtsuxk.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY_HERE'; // *** ใส่ anon key ของคุณที่นี่ ***
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5teWtkZW5kam10dGp2dnRzdXhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3Mzk4MTksImV4cCI6MjA2ODMxNTgxOX0.gp1hzku2fDBH_9PvMsDCIwlkM0mssuke40smgU4-paE'; // *** ใส่ anon key ของคุณที่นี่ ***
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
